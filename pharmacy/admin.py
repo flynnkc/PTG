d0143209ed5866b869161b0628edc48587258ae8
Peter Jefferson <peterjefferson@wireless-nat-inside.uiowa.edu>
2019-03-26 11:45:28 -0500

    Updated Models.py, Admin.py

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -3,5 +3,11 @@ from . import models
 
 # Register models to add to admin page
 # If model in models.py do not have __str__ functions they will display as generic objects
-admin.register(models.Drug_Generic)
-admin.register(models.Drug_Brand)
+admin.site.register(models.Drug_Generic)
+admin.site.register(models.Drug_Brand)
+admin.site.register(models.Manufacturer_Company)
+admin.site.register(models.Location)
+admin.site.register(models.Order)
+admin.site.register(models.Batch)
+admin.site.register(models.Order_Detail)
+admin.site.register(models.Batch_Location)
--- pharmacy/models.py
+++ pharmacy/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# This file is for models
-# Reference https://docs.djangoproject.com/en/2.1/topics/db/models/
 class Drug_Generic(models.Model):
     name = models.CharField(primary_key=True, max_length=50)
     schedule = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-
 
 class Manufacturer_Company(models.Model):
     name = models.CharField(max_length=100)
@@ -19,45 +16,59 @@ class Manufacturer_Company(models.Model):
     def __str__(self):
         return self.name
 
-
 class Drug_Brand(models.Model):
     name = models.CharField(primary_key=True, max_length=50)
     generic_name = models.ForeignKey(Drug_Generic, on_delete=models.CASCADE)
     manufacturer = models.ForeignKey(Manufacturer_Company, on_delete=models.CASCADE)
-    licence = models.IntegerField()
+    license = models.IntegerField()
 
     def __str__(self):
         return self.name
 
-
 class Location(models.Model):
     name = models.CharField(max_length=80)
+    upstream_supplier = models.IntegerField(null=True, blank=True)
     is_hospital = models.BooleanField(default=False)
     is_clinic = models.BooleanField(default=False)
     address = models.TextField(null=True, blank=True)
     phone = models.CharField(max_length=14)
 
-    def __str(self):
+    def __str__(self):
         return self.name
 
+class Order(models.Model):
+    location_from = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='location')
+    location_to = models.ForeignKey(Location, on_delete=models.CASCADE, related_name ='loaction')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #MODIFY DATE FIELD TO FORMAT DESIRED BY RAPULA
+    date_time = models.DateField()
+    state = models.CharField(max_length=50)
 
-# Use barcode for the primary key on Drug model
-class Drug(models.Model):
-    barcode = models.IntegerField(primary_key=True)
-    name = models.ForeignKey(Drug_Brand, on_delete=models.CASCADE)
-    location = models.ManyToManyField(Location)
-    batch_lot = models.IntegerField()
-    dose = models.CharField(max_length=20, blank=True)
-    count = models.IntegerField()
+    def __str__(self):
+        return self.state
+
+#ORDER_DETAIL ASSOCIATIVE ENTITY ATTEMPT
+# https://stackoverflow.com/questions/28712848/composite-primary-key-in-django/28712960#28712960
+class Order_Detail(models.Model):
+    class Meta:
+        unique_together = (('order_id','barcode'),)
+    order_id = models.IntegerField(primary_key=True)
+    barcode = models.IntegerField()
+
+class Batch(models.Model):
+    drug_brand = models.ForeignKey(Drug_Brand, on_delete=models.CASCADE)
+    #MODIFY DATE FIELD TO FORMAT DESIRED BY RAPULA
     expiration_date = models.DateField()
-    checked_out = models.BooleanField(null=True, blank=True)
-    reason = models.TextField(null=True, blank=True)
+    #Batch size is allowed to be null, change if it needs to have the batch size.
+    batch_size = models.IntegerField(null=True, blank=True)
 
+    def __str__(self):
+        return self.drug_brand
 
-class Receive(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
-
-
-class Disburse(models.Model):
-    pass
+#Batch_Location ASSOCIATIVE ENTITY ATTEMPT
+# https://stackoverflow.com/questions/28712848/composite-primary-key-in-django/28712960#28712960
+class Batch_Location(models.Model):
+    class Meta:
+        unique_together = (('batch_id','location_id'),)
+    batch_id = models.IntegerField(primary_key=True)
+    location_id = models.IntegerField()
